447dbbf2046fb68bbab1749c82372651ac2ea3bc
IrynaBarnych <iryna.barnych@gmail.com>
2023-12-04 19:36:12 +0200

    #черга через бібліотеку колекції

--- main.py
+++ main.py
@@ -8,3 +8,16 @@ queue.append(queue.pop(0)) #кругова черга (Ring Buffer)
 print(queue)
 
 
+#черга через бібліотеку колекції
+from collections import deque
+q = deque()
+
+q.append("a")
+q.append("b")
+q.append("c")
+print(q)
+q.popleft()
+q.popleft()
+q.popleft()
+q.rotate(2)
+print(q)
